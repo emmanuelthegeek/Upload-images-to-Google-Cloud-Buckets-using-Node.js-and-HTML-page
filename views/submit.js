8f0d6eb5b20c146fb0d2aa03f52c4022e5e30a82
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-09-22 23:43:34 +0100

    first push

--- views/submit.js
+++ views/submit.js
@@ -12,7 +12,6 @@ document.getElementById("submitBtn").addEventListener("click", () => {
   let postid = uuidv4();
   let inputElem = document.getElementById("imgfile");
   let file = inputElem.files[0];
-  // Create new file so we can rename the file
   let blob = file.slice(0, file.size, "image/jpeg");
   newFile = new File([blob], `${postid}_post.jpeg`, { type: "image/jpeg" });
   // Build the form data - You can add other input values to this i.e descriptions, make sure img is appended last
