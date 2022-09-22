cdf87014f6868965f2b159c42b01ec8b254b467d
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-09-22 23:44:56 +0100

    first push

--- views/submit.js
+++ views/submit.js
@@ -1,4 +1,3 @@
-// Client side unique ID - This could and probably should move to server with UUID
 function uuidv4() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (
