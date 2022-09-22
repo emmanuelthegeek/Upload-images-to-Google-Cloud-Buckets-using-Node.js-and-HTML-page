71c22c00bc45e9da3e70763cc2af2f3307ec9820
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-09-22 23:45:39 +0100

    first push

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(src));
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // No larger than 5mb, change as you need
+    fileSize: 5 * 1024 * 1024, 
   },
 });
 
